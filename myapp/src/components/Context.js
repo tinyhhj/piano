60514dc626dc60ed1ec56ca533a1fabb071c7d57
tinyhhj <krwsim@gmail.com>
2019-10-26 17:53:29 +0900

    2019-10-26 도커파일 추가 및 권한변경

--- myapp/src/components/Context.js
+++ myapp/src/components/Context.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const hostContext = React.createContext('http://hellobluejoy.iptime.org/:8080');
+const hostContext = React.createContext('http://hellobluejoy.iptime.org:8080');
 // const hostContext = React.createContext('http://localhost:8080');
 
 export default hostContext;
