9289ad377153a01a29edd6f0e9c6034a6b00a8b1
tinyhhj <krwsim@gmail.com>
2020-05-16 06:24:22 +0900

    compose file add

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ cd myapp
 npm run build
 cd ../
 ./mvnw install -Dmaven.test.skip=true
+../yt_web_down/build.sh
+docker image pull linuxserver/transmission
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-docker-compose -f docker-compose.yml -f docker-compose-nginx.yml up -d --build
+#!/bin/bash
+
+if [ 'build' == "$1" ]; then
+docker-compose -f docker-compose.yml -f docker-compose-nginx.yml -f docker-compose-transmission.yml -f docker-compose-yt-web.yml -f docker-compose-yt-worker.yml up -d --build
+else
+docker-compose -f docker-compose.yml -f docker-compose-nginx.yml -f docker-compose-transmission.yml -f docker-compose-yt-web.yml -f docker-compose-yt-worker.yml up -d
+fi
 
